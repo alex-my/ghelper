9ebb1b371e96058acda3c72dd80c96cbcbec9902
alex <alex_my@126.com>
2019-09-28 17:24:24 +0800

    移除尚未实现的文件

--- fuse/fuse.go
+++
@@ -1,5 +0,0 @@
-package fuse
-
-// TODO: 熔断器
-// 参考:
-// gweb-examples/personal/_cn/dotweb/framework/hystrix
--- logger/zap.go
+++
@@ -1,4 +0,0 @@
-package logger
-
-// TODO: 封装 zap 日志
-// zap: https://github.com/uber-go/zap
