9197849bde60c3c6740e710ee08da032d8f6b2df
alex <alex_my@126.com>
2019-10-24 14:53:52 +0800

    增加签名辅助

--- main.go
+++ main.go
@@ -3,32 +3,25 @@ package main
 import (
 	"fmt"
 
-	"github.com/alex-my/ghelper/cache"
+	"github.com/alex-my/ghelper/sign"
 )
 
 func main() {
-	c := cache.NewCache(
-		cache.WithPort(11234),
-		cache.WithPassword("uio876..."),
-	)
+	testValue()
+	testValues()
+}
 
-	err := c.Open()
-	if err != nil {
-		fmt.Printf("open failed: %s\n", err.Error())
-		return
+func testValue() {
+	params := map[string]string{
+		"timestamp": "1571747084",
+		"username":  "root",
+		"password":  "123456",
 	}
 
-	r1, err := c.SetString("a", "a0", "3000")
-	if err != nil {
-		fmt.Printf("setstring failed: %s\n", err.Error())
-		return
-	}
-	fmt.Printf("setstring result, r1: %v\n", r1)
+	calcSign, _ := sign.Sign(params)
+	fmt.Printf("calcSign: %s\n", calcSign)
+}
 
-	r2, err := c.String("a")
-	if err != nil {
-		fmt.Printf("getstring failed: %s\n", err.Error())
-		return
-	}
-	fmt.Printf("getstring result, r2: %v\n", r2)
+func testValues() {
+
 }
---
+++ sign/sign.go
@@ -0,0 +1,150 @@
+package sign
+
+import (
+	"bytes"
+	"errors"
+	"sort"
+	"sync"
+
+	"github.com/alex-my/ghelper/crypto"
+	"github.com/alex-my/ghelper/logger"
+)
+
+var (
+	// ErrorInvalidSignParams 无效的签名参数
+	ErrorInvalidSignParams = errors.New("invalid sign param")
+)
+
+var (
+	// ignoreField 不参与签名的字段
+	ignoreField = "sign"
+	// secretKey 密钥
+	secretKey = "abcdefg"
+	// calcFunc 签名函数
+	calcFunc = calc
+	// log 日志
+	log = logger.NewLogger()
+	// debug 是否打印日志
+	debug = true
+)
+
+// SetIgnoreField 设置忽略的字段
+func SetIgnoreField(field string) {
+	ignoreField = field
+}
+
+// SetSecretKey 设置签名密钥
+func SetSecretKey(key string) {
+	secretKey = key
+}
+
+// SetCalcFunc 设置签名函数
+func SetCalcFunc(f func(string) string) {
+	if f == nil {
+		return
+	}
+	calcFunc = f
+}
+
+// SetLog 设置日志
+func SetLog(l logger.Logger) {
+	log = l
+}
+
+// SetDebug 是否打印日志
+func SetDebug(b bool) {
+	debug = b
+}
+
+// Signs 计算签名
+func Signs(values map[string][]string) (string, error) {
+	if values == nil {
+		return "", ErrorInvalidSignParams
+	}
+
+	v := map[string]string{}
+	for name, value := range values {
+		if len(value) > 0 {
+			v[name] = value[0]
+		}
+	}
+
+	return sign(v)
+}
+
+// Sign 计算签名
+func Sign(values map[string]string) (string, error) {
+	if values == nil {
+		return "", ErrorInvalidSignParams
+	}
+
+	return sign(values)
+}
+
+// sign 计算签名过程
+func sign(values map[string]string) (string, error) {
+
+	// 所有参数按照字母顺序从小到大排列，参数值为空不参与签名
+	// 所有参数形成如 key1=value1&key2=value2 的形式
+	size := len(values)
+	keys := make([]string, 0, size)
+	for key, value := range values {
+		if key != ignoreField && value != "" {
+			keys = append(keys, key)
+		}
+	}
+
+	sort.Strings(keys)
+
+	b := buffer()
+	defer releaseBuffer(b)
+	b.Reset()
+
+	size = len(keys)
+	for index, key := range keys {
+		if key == "" {
+			continue
+		}
+		value := values[key]
+
+		b.WriteString(key)
+		b.WriteString("=")
+		b.WriteString(value)
+
+		if index != size-1 {
+			b.WriteString("&")
+		}
+	}
+
+	signStr := b.String()
+
+	return calcFunc(signStr), nil
+}
+
+// calc 默认的签名函数
+func calc(s string) string {
+	out := crypto.Md5(s + secretKey)
+	if debug {
+		log.Debugf("signStr: %s, calcSign: %s", s, out)
+	}
+	return out
+}
+
+var bufferPool *sync.Pool
+
+func buffer() *bytes.Buffer {
+	buff := bufferPool.Get().(*bytes.Buffer)
+	buff.Reset()
+	return buff
+}
+
+func releaseBuffer(buff *bytes.Buffer) {
+	bufferPool.Put(buff)
+}
+
+func init() {
+	bufferPool = &sync.Pool{}
+	bufferPool.New = func() interface{} {
+		return &bytes.Buffer{}
+	}
+}
