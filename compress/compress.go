26f7d814bba7ed07b0771506d0d83a75090bcb00
alex <alex_my@126.com>
2020-09-01 16:20:55 +0800

    修改压缩与解压接口

--- compress/compress.go
+++ compress/compress.go
@@ -1,44 +1,13 @@
 package compress
 
-import (
-	"bytes"
-	"compress/gzip"
-	"io/ioutil"
-)
+// Compress 压缩接口
+type Compress interface {
+	// Compress 压缩
+	Compress(in []byte) ([]byte, error)
 
-var defaultLevel = gzip.DefaultCompression
+	// Uncompress 解压缩
+	Uncompress(in []byte) ([]byte, error)
 
-// SetLevel 设置压缩级别
-func SetLevel(level int) {
-	defaultLevel = level
-}
-
-// GzipCompress gzip 压缩
-func GzipCompress(in []byte) ([]byte, error) {
-	var buffer bytes.Buffer
-
-	writer, err := gzip.NewWriterLevel(&buffer, defaultLevel)
-	if err != nil {
-		return nil, err
-	}
-	defer writer.Close()
-
-	_, err = writer.Write(in)
-	if err != nil {
-		return nil, err
-	}
-
-	return buffer.Bytes(), nil
-}
-
-// GzipUncompress gzip 解压
-func GzipUncompress(in []byte) ([]byte, error) {
-	reader, err := gzip.NewReader(bytes.NewReader(in))
-	if err != nil {
-		return nil, err
-	}
-
-	defer reader.Close()
-
-	return ioutil.ReadAll(reader)
+	// Name 获取名称
+	Name() string
 }
