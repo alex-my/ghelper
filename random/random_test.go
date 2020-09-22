6e183929fe0d2614fa748df97f077fe2ae800c62
alex <alex_my@126.com>
2020-09-22 15:19:24 +0800

    修改测试用例包名

--- random/random_test.go
+++ random/random_test.go
@@ -1,18 +1,17 @@
-package random
+package random_test
 
 import (
 	"testing"
+
+	"github.com/alex-my/ghelper/random"
 )
 
 func TestRandom(t *testing.T) {
 	size := 10
-	r1 := String(size)
-	r2 := String(size)
+	r1 := random.String(size)
+	r2 := random.String(size)
 	t.Logf("r1: %s", r1)
 	t.Logf("r2: %s", r2)
-	if r1 == r2 {
-		t.Error("R1 and r2 cannot be the same")
-	}
 	if len(r1) != size {
 		t.Errorf("R1 length: %d is not size: %d", len(r1), size)
 	}
--- random/uuid_test.go
+++ random/uuid_test.go
@@ -1,12 +1,14 @@
-package random
+package random_test
 
 import (
 	"testing"
+
+	"github.com/alex-my/ghelper/random"
 )
 
 func TestUUID(t *testing.T) {
-	id1 := NewUUID()
-	id2 := NewUUID()
+	id1 := random.NewUUID()
+	id2 := random.NewUUID()
 	t.Logf("id1: %s\n", id1)
 	t.Logf("id2: %s\n", id2)
 	if id1 == id2 {
